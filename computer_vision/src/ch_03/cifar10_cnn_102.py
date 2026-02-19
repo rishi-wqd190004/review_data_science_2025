2445b1db973da5a99b9cd9f437c177c8879519a8
rishi-wqd190004 <rishinigam1304@gmail.com>
2026-02-19 14:33:30 +0530

    adding training loop and val loop using mini Alex Net

--- computer_vision/src/ch_03/cifar10_cnn_102.py
+++ computer_vision/src/ch_03/cifar10_cnn_102.py
@@ -5,6 +5,18 @@ from torch.utils.data import random_split
 
 generator = torch.Generator().manual_seed(42)
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Example with MPS (Apple Silicon) support
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+elif torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+print(f"Using device: {device}")
+
 transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize(
@@ -26,9 +38,9 @@ val_size = len(full_set) - train_size
 
 train_set, val_set = random_split(full_set, [train_size, val_size], generator=generator)
 
-trainloader = torch.utils.data.DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True, num_workers=2)
+trainloader = torch.utils.data.DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True, num_workers=0)
 
-valloader = torch.utils.data.DataLoader(dataset=val_set, batch_size=batch_size, shuffle=True, num_workers=2)
+valloader = torch.utils.data.DataLoader(dataset=val_set, batch_size=batch_size, shuffle=True, num_workers=0)
 
 testset = CIFAR10(
             root='./data',
@@ -36,7 +48,59 @@ testset = CIFAR10(
             download=True,
             transform=transform
             )
-testloader = torch.utils.data.DataLoader(dataset=testset, batch_size=batch_size, shuffle=False, num_workers=2)
+testloader = torch.utils.data.DataLoader(dataset=testset, batch_size=batch_size, shuffle=False, num_workers=0)
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+def train_step(model: torch.nn.Module, data_loader: torch.utils.data.DataLoader, loss_fn: torch.nn.Module, optimizer: torch.optim.Optimizer, accuracy_metric, device: torch.device=device):
+    train_loss = 0
+    accuracy_metric.reset()
+    model.to(device)
+    for batch, (x,y) in enumerate(data_loader):
+        x, y = x.to(device), y.to(device)
+        # 1. forward pass
+        y_pred = model(x)
+        # 2. calculate loss
+        loss = loss_fn(y_pred, y)
+        train_loss += loss.item()
+        # 3. optimizer zero grad
+        optimizer.zero_grad()
+        # 4. loss backward
+        loss.backward()
+        # 5. optimizer step
+        optimizer.step()
+
+        preds = torch.argmax(y_pred, dim=1)
+        accuracy_metric.update(preds, y)
+
+    # per epoch train loss and accuracy
+    train_loss /= len(data_loader)
+    train_acc = accuracy_metric.compute()
+
+    print(f"Train loss: {train_loss:.5f} | Train accuracy: {train_acc:.2f}%")
+
+def test_step(model: torch.nn.Module, data_loader: torch.utils.data.DataLoader, loss_fn: torch.nn.Module, accuracy_metric, device: torch.device=device, valid: bool=True):
+    test_loss = 0
+    accuracy_metric.reset()
+    model.to(device)
+    model.eval()
+    # inference context manager
+    with torch.inference_mode():
+        for x, y in data_loader:
+            x, y = x.to(device), y.to(device)
+            # 1. forward pass
+            test_pred = model(x)
+            # 2. train and loss accuracy
+            loss = accuracy_metric(test_pred, y)
+            test_loss += loss.item()
+            preds = torch.argmax(test_pred, dim=1)
+            accuracy_metric.update(preds, y)
+
+        # per epoch train loss and accuracy
+        test_loss /= len(data_loader)
+        test_acc = accuracy_metric.compute()
+        if valid:
+            print(f"Val loss: {test_loss:.5f} | Val accuracy: {test_acc:.2f}%\n")
+        else:
+            print(f"Test loss: {test_loss:.5f} | Test accuracy: {test_acc:.2f}%\n")
